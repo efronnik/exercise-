28cc26c8495760f4a9f41c6b6b4abab221955a00
Nikita <nikitaefron06@gmail.com>
2023-11-28 21:04:10 +0100

    Summary (required)

---
+++ exercise/exercise-1.js
@@ -0,0 +1,19 @@
+let firstName = 'Nikita';
+let lastName = 'Poddubnyi';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// let firstName = 'Nikita';
+// let lastName = 'Poddubnyi';
+// let fullName = `${firstName} ${lastName}`;
+// console.log(fullName);
+
+// const firstName = 'Nikita';
+// const lastName = 'Poddubnyi';
+// const fullName = firstName.concat(' ', lastName);
+// console.log(fullName);
+
+// const firstName = 'Nikita';
+// const lastName = 'Poddubnyi';
+// const fullName = [firstName, lastName].join(' ');
+// console.log(fullName);
---
+++ exercise/exercise-2.js
@@ -0,0 +1,15 @@
+let numString = '42';
+let num = Number(numString);
+console.log(num);
+
+// let numString = '42';
+// let num = parseInt(numString, 10); // Второй параметр - основание системы счисления (в данном случае, десятичная система)
+// console.log(num);
+
+// let numString = '42.5';
+// let num = parseFloat(numString);
+// console.log(num);
+
+// let numString = '42';
+// let num = +numString;
+// console.log(num);

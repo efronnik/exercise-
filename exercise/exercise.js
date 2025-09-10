f2e267059448e914d1171950c8e0e9f3eff14f94
Nikita <nikitaefron06@gmail.com>
2025-09-10 16:39:32 +0200

    Summary (required)

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -58,17 +58,58 @@
 // «Вы взрослый», если возраст ≥ 20
 // «Вы ребенок», если возраст < 13
 
-const ageChild = Number(prompt('Введите ваш возраст!'));
+// const ageChild = Number(prompt('Введите ваш возраст!'));
 
-function seeYourAge() {
-  if (ageChild >= 13 && ageChild <= 19) {
-    return 'Вы подросток';
-  } else if (ageChild >= 20) {
-    return 'Вы взрослый';
-  } else {
-    return 'Вы ребенок';
-  }
-}
+// function seeYourAge() {
+//   if (ageChild >= 13 && ageChild <= 19) {
+//     return 'Вы подросток';
+//   } else if (ageChild >= 20) {
+//     return 'Вы взрослый';
+//   } else {
+//     return 'Вы ребенок';
+//   }
+// }
 
-const result = seeYourAge();
-alert(result);
+// const result = seeYourAge();
+// alert(result);
+
+// task 8 Проверка логики:
+// 	Пользователь вводит число. Выведите «Число положительное и четное» только если это число > 0 и делится на 2.
+// const pushNumber = Number(prompt('Введите число'));
+
+// if (isNaN(pushNumber)) {
+//   alert('Это не число!');
+// } else if (pushNumber % 2 === 0 && pushNumber > 0) {
+//   alert('Четное положительное число');
+// } else {
+//   alert('Число либо нечетное, либо отрицательное, либо ноль');
+// }
+
+// task 9 Выберите опцию:
+// 1 — Привет
+// 2 — Пока
+// // 3 — Случайное число
+
+// switch (
+//   Number(prompt('Выберите опцию:\n1 - Привет\n2 - Пока\n3 - Случайное число'))
+// ) {
+//   case 1:
+//     alert('Привет!');
+//     break;
+//   case 2:
+//     alert('Пока!');
+//     break;
+//   case 3:
+//     alert(Math.random());
+//     break;
+//   default:
+//     alert('Нет такой опции!');
+// }
+
+// Task 10
+// Тернарный оператор: Пусть пользователь вводит число.
+// Выведите «Число положительное» или «Число отрицательное» через тернарный оператор.
+
+const addNumber = Number(prompt('Введите число!'));
+
+alert(addNumber > 0 ? 'Число положительное' : 'Число отрицательное');
